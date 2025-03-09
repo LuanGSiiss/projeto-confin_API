e7e8c5fb62e7c7663f1635c736a5cfd43b60d05a
Luan Gerber Siiss <luangerber666@gmail.com>
2025-03-09 15:24:39 -0300

    parte 1

--- ConFinServer/Model/Estado.cs
+++ ConFinServer/Model/Estado.cs
@@ -1,9 +1,20 @@
-﻿namespace ConFinServer.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConFinServer.Model
 {
     public class Estado
     {
+        [Key]
+        [StringLength(2
+                     , MinimumLength = 2
+                    , ErrorMessage = "Obrigatório informar 2 caracteres")]
         public string Sigla { get; set; }
-        
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [StringLength(100
+                     , MinimumLength = 2
+                     , ErrorMessage = "Informe de 2 a 100 caracteres"
+            )]
         public string Nome { get; set; }
 
     }
