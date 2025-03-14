adc0754c8abe662efe80d3c695ec3ff41efdb534
Luan Gerber Siiss <1033720@UNIDAVI.LOCAL>
2025-03-13 21:38:37 -0300

    ajustado cidade

--- ConFinServer/Model/Cidade.cs
+++ ConFinServer/Model/Cidade.cs
@@ -1,10 +1,24 @@
-﻿namespace ConFinServer.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace ConFinServer.Model
 {
     public class Cidade
     {
+        [Key]
+        [Range(1, int.MaxValue, ErrorMessage = "Código não pode ser negativo")]
         public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [StringLength(100
+                     , MinimumLength = 2
+                     , ErrorMessage = "Informe de 2 a 100 caracteres"
+            )]
         public string Nome { get; set; }
 
+        [StringLength(2
+                     , MinimumLength = 2
+                    , ErrorMessage = "Obrigatório informar 2 caracteres")]
         public string Estado { get; set; }
     }
 }
