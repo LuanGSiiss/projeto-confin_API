2ec04757d1f529166992a4a465517a2c6942b907
Luan Gerber Siiss <luangerber666@gmail.com>
2025-05-11 21:29:46 -0300

    trazendo os depois modelos, conta e pessoa

---
+++ ConFinServer/Model/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConFinServer.Model
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [StringLength(100
+                     , MinimumLength = 2
+                     , ErrorMessage = "Informe de 2 a 100 caracteres"
+            )]
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Cidade é obrigatório")]
+        public int CidadeCodigo { get; set; }
+
+        public Cidade? Cidade { get; set; }
+    }
+}
